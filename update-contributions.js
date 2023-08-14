7c2e0e118443bf133afab49b47d911b47cca3739
TurkLirasiBOT <140286722+TurkLirasiBOT@users.noreply.github.com>
2023-08-14 18:49:53 +0300

    Create update-contributions.js

---
+++ update-contributions.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const fs = require('fs');
+
+async function updateContributionsCount() {
+  try {
+    const username = 'cemobaskann'; // GitHub kullanıcı adınız
+    const response = await axios.get(`https://api.github.com/users/${username}/contributions`);
+
+    const contributionsCount = response.data.total_count;
+
+    const readmeContent = fs.readFileSync('README.md', 'utf8');
+    const updatedReadmeContent = readmeContent.replace(
+      /(?<=\bKatkılarım: )\d+/,
+      contributionsCount.toString()
+    );
+
+    fs.writeFileSync('README.md', updatedReadmeContent);
+    console.log('README.md güncellendi.');
+  } catch (error) {
+    console.error('Hata:', error.message);
+  }
+}
+
+updateContributionsCount();
